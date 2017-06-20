339330c46c0211e16e202a19c491484e11186a41
John Lamb <jklamb@iupui.edu>
2017-06-19 23:25:01 -0400

    add new note when the form is submitted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
-
 import Main from './Main'
 
 class App extends Component {
@@ -10,36 +8,18 @@ class App extends Component {
     super()
 
     this.state = {
-      notes: {
-        'notes-1': {
-          id: 'note-1',
-          title: 'Thoughts on React',
-          body: 'React is pretty nifty.'
-        },
-        'note-2': {
-          id: 'note-2',
-          title: 'State and Props?',
-          body: 'Wat.',
-        },
-        'note-317': {
-          id: 'note-3',
-          title: 'naptown',
-          body: 'stand up',
-        }
-      },
+      notes: {},
     }
-
   }
 
   saveNote = (note) => {
-    if(!note.id) {
+    if (!note.id) {
       note.id = `note-${Date.now()}`
     }
-    const notes = { ...this.state.notes }
-    notes[note.Id] = note
+    const notes = {...this.state.notes}
+    notes[note.id] = note
     this.setState({ notes })
   }
-
 
   render() {
     return (
